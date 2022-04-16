62141a4461c6749d2914d87847f4c796fd4d433e
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-04-16 16:07:03 +0200

    Fix CartProduct constructor

--- src/js/script.js
+++ src/js/script.js
@@ -474,6 +474,11 @@
     constructor(menuProduct, element){
       const thisCartProduct = this;
       thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.params = menuProduct.params; 
       
       thisCartProduct.getElements(element);
       console.log('new Cart product', thisCartProduct);
