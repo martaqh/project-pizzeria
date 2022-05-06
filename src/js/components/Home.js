c7aead3c60f96d439e8eef782662116808d26680
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-05-06 16:44:14 +0200

    Activate links from homepage to order and booking

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -5,18 +5,54 @@ class Home {
     const thisHome = this;
 
     thisHome.render(element);
+    thisHome.initLinks();
 
   }
 
   render(element){
     const thisHome = this;
     const generatedHTML = templates.homePage();
-    console.log(generatedHTML);
 
     thisHome.dom = {};
     thisHome.dom.wrapper = element;
     thisHome.dom.wrapper.innerHTML = generatedHTML;
+    thisHome.dom.orderLink = document.querySelector('.link-to-order');
+    console.log(thisHome.dom.orderLink);
+    thisHome.dom.bookingLink = document.querySelector('.link-to-booking');
+    thisHome.dom.orderNavLink = document.querySelector('a[href="#order"]');
+    console.log( thisHome.dom.orderSubpage);
+    thisHome.dom.bookingNavLink = document.querySelector('a[href="#booking"]');
+    thisHome.dom.homeNavLink = document.querySelector('a[href="#home"]');
+    thisHome.dom.orderSubpage = document.querySelector('#order');
+    thisHome.dom.homeSubpage = document.querySelector('#home');
+    thisHome.dom.bookingSubpage = document.querySelector('#booking');
+    console.log(thisHome.dom.orderSubpage);
+  }
 
+  initLinks(){
+
+    const thisHome = this;
+    
+
+    thisHome.dom.orderLink.addEventListener('click', function(event){
+      
+      event.preventDefault();
+      thisHome.dom.orderNavLink.classList.add('active');
+      thisHome.dom.homeNavLink.classList.remove('active');
+      window.location.hash = '#/order';
+      thisHome.dom.orderSubpage.classList.add('active');
+      thisHome.dom.homeSubpage.classList.remove('active');
+
+      console.log('classes added/taken');
+    });
+
+    thisHome.dom.bookingLink.addEventListener('click', function(){
+      thisHome.dom.bookingNavLink.classList.add('active');
+      thisHome.dom.homeNavLink.classList.remove('active');
+      window.location.hash = '#/booking';
+      thisHome.dom.bookingSubpage.classList.add('active');
+      thisHome.dom.homeSubpage.classList.remove('active');
+    });
 
   }
 }
